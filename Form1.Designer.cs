03a90c27c52681e696e047dda1ae07e4c96b0f13
ismailkara3d <ikara3d@gmail.com>
2019-06-15 06:12:28 +0300

    Selection Sort

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace selectionSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btnSirala = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(248, 75);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Diziye Ekle";
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(132, 75);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(100, 22);
+            this.txtSayi.TabIndex = 2;
+            // 
+            // btnSirala
+            // 
+            this.btnSirala.Location = new System.Drawing.Point(366, 75);
+            this.btnSirala.Name = "btnSirala";
+            this.btnSirala.Size = new System.Drawing.Size(75, 23);
+            this.btnSirala.TabIndex = 0;
+            this.btnSirala.Text = "Sırala";
+            this.btnSirala.UseVisualStyleBackColor = true;
+            this.btnSirala.Click += new System.EventHandler(this.btnSirala_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1513, 741);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSirala);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btnSirala;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace selectionSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> sayilar = new List<int>();
+        List<Button> butonlar = new List<Button>();
+        List<Button> siraliButon = new List<Button>();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            if (int.TryParse(txtSayi.Text, out a))
+            {
+                sayilar.Add(int.Parse(txtSayi.Text));
+                DiziOlustur(int.Parse(txtSayi.Text));
+            }
+            txtSayi.Clear();
+        }
+
+        void DiziOlustur(int sayi)
+        {
+
+            Button btn = new Button();
+            btn.Name = sayi.ToString();
+            btn.SetBounds(butonlar.Count > 0 ? butonlar.Last().Left + 75 : 50, 200, 50, 50);
+            btn.Text = sayi.ToString();
+            btn.Show();
+            butonlar.Add(btn);
+            this.Controls.Add(btn);
+        }
+
+        void ButonOlustur(List<int> dizi, int top)
+        {
+
+            for (int i = 0; i < siraliButon.Count; i++)
+            {
+                this.Controls.Remove(siraliButon[0]);
+            }
+            siraliButon.Clear();
+            for (int i = 0; i < dizi.Count; i++)
+            {
+                Button btn = new Button();
+                btn.Name = dizi[i].ToString();
+                btn.SetBounds(siraliButon.Count > 0 ? siraliButon.Last().Left + 75 : 50, top, 50, 50);
+                btn.Text = dizi[i].ToString();
+                btn.Show();
+                siraliButon.Add(btn);
+                siraliButon[i].BackColor = Color.FromArgb(i * (128 / dizi.Count) + 100, 20, 20);
+                this.Controls.Add(btn);
+
+            }
+
+
+        }
+  
+        void SecmeSiralamasi(List<int> liste)
+        {
+
+            int temp = 0;
+            for (int i = 0; i < liste.Count; i++)
+            {
+
+                for (int j = i + 1; j < liste.Count; j++)
+                {
+                   
+                    if (liste[j] < liste[i])
+                    {                       
+                        temp = liste[i];
+                        liste[i] = liste[j];
+                        liste[j] = temp;                        
+                    }
+               
+                   
+                }
+            }
+            ButonOlustur(liste, 300);
+        }
+
+        private void btnSirala_Click(object sender, EventArgs e)
+        {
+            SecmeSiralamasi(sayilar);           
+        }
+    }
+}
